094034273483428208bd0fa9d1fa2b6d4bc69f75
Rohitchoudhary7060 <rohitchoudhary.goldy@gmail.com>
2026-05-30 15:21:10 +0530

    Update backend.groovy

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -38,7 +38,7 @@ pipeline {
             }
         }
         stage('DOCKER BUILD') {
-            steps { withDockerRegistry([credentialsId:'docker']) {
+            steps { withDockerRegistry([credentialsId:'docker',url: 'https://index.docker.io/v1/']) {
 
                     sh '''
                         cd FlightReservationApplication
