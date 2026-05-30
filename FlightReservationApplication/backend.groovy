95de2d19536298d906dbf0e4d336736f4072067b
Rohitchoudhary7060 <rohitchoudhary.goldy@gmail.com>
2026-05-30 15:48:11 +0530

    Update backend.groovy

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -45,9 +45,8 @@ pipeline {
                         docker build -t rohit7060/flight-reservation:latest .
                         docker push rohit7060/flight-reservation:latest
                         docker rmi rohit7060/flight-reservation:latest
-                        '''
+                      '''
                 }
-
             }
         }
      }
