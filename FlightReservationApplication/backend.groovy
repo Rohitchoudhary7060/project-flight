aaf7d78ec4e0211d430d6b60e105136366398620
Rohitchoudhary7060 <rohitchoudhary.goldy@gmail.com>
2026-05-30 14:22:12 +0530

    Update backend.groovy

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -24,7 +24,7 @@ pipeline {
                     sh '''
                         cd FlightReservationApplication
                         mvn sonar:sonar \
-                        -Dsonar.projectKey=flight-reservation \ 
+                        -Dsonar.projectKey=flight-reservation \
                         -Dsonar.projectName=flight-reservation
                     '''
                 }
